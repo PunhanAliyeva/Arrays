141b1a83f792b54b57d9c5d8f4e6e37071b126ff
PunhanAliyeva <eliyevapunhan567@gmail.com>
2024-03-07 10:53:59 +0400

    task is completed

---
+++ ArrayTask2/ArrayTask2/Program.cs
@@ -0,0 +1,95 @@
+﻿#region Task1
+
+//int[] nums = { 4, 2, 9, 11, 27, 5, 1, 19 };
+//Console.WriteLine("Eded daxil edin..");
+//string strNum = Console.ReadLine();
+//int num = Convert.ToInt32(strNum);
+//bool IsCheck=false;
+//foreach (int i in nums)
+//{
+//    if (num < i)
+//    {
+//        IsCheck = true;
+//        break;
+//    }
+//}
+//Console.WriteLine(IsCheck);
+
+#endregion
+
+
+#region Task2
+//int number = 45690;
+//string strNum = number.ToString();
+//foreach (var num in strNum)
+//{
+//    if (num % 2 == 0)
+//    {
+//        Console.WriteLine(num);
+//    }
+//}
+#endregion
+
+
+#region Task3
+//int[] numbers = { 6, 13, 90, 46, 27, 13, 8 };
+//int sum = 0;
+//int count = 0;
+//for (int i = 0; i < numbers.Length; i+=2)
+//{
+//    sum += numbers[i];
+//    count++;
+//}
+//Console.WriteLine(sum/count);
+#endregion
+
+
+#region Task4
+//int[] nums = { 3, 19, 4, 28, 59, 37, 9, 12 };
+//for (int i = nums.Length-1; i >= 0; i--)
+//{
+//    Console.WriteLine(nums[i]);
+//}
+#endregion
+
+
+#region Task5
+
+//int[] arr = { 7914,342, 890, 77, 54, 6, 11 };
+//foreach (var num in arr)
+//{
+//    string strNum =num.ToString();
+//    Console.WriteLine(strNum.Length);
+//}
+#endregion
+
+
+#region Task6
+
+//int[] nums = { 5, 2, 8, 11, 24, 3, 9, 1 };
+//int maxValue = nums[0];
+//foreach (int num in nums)
+//{
+//    if (num>maxValue)
+//    {
+//        maxValue =num;
+//    }
+//}
+//Console.WriteLine(maxValue);
+#endregion
+
+
+#region Task7
+
+//int[] nums = { 6, -19, 26, 38, 2, 9, 10, -1 };
+//int minValue = nums[0];
+//foreach (int num in nums)
+//{
+//    if (num < minValue)
+//    {
+//        minValue = num;
+//    }
+//}
+//Console.WriteLine(minValue);
+
+#endregion
